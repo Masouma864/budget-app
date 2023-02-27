069c83005f482369fec9d1174eb08f07c23df6d5
masouma <masouma2020@gmail.com>
2023-02-27 18:19:33 +0430

    Modify rubocop.yml and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_227_122_605) do
+ActiveRecord::Schema[7.0].define(version: 20_230_227_132_723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'expenses', force: :cascade do |t|
+    t.string 'name'
+    t.decimal 'amount'
+    t.bigint 'author_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_expenses_on_author_id'
   end
 
   create_table 'group_expenses', force: :cascade do |t|
@@ -29,15 +33,22 @@ ActiveRecord::Schema[7.0].define(version: 20_230_227_122_605) do
   end
 
   create_table 'groups', force: :cascade do |t|
+    t.string 'name'
+    t.string 'icon', default: 'https://th.bing.com/th/id/R.bbd92d64f5c944c8deb186f221857c0e?rik=qkpNCm0avpbgcA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_317099.png&ehk=ku2fAarDstuuq2N4Mi6LoH%2b2xZf0aTKTtW5FHlDfymE%3d&risl=&pid=ImgRaw&r=0'
+    t.bigint 'author_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_groups_on_author_id'
   end
 
   create_table 'users', force: :cascade do |t|
+    t.string 'name'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
 
+  add_foreign_key 'expenses', 'users', column: 'author_id'
   add_foreign_key 'group_expenses', 'expenses'
   add_foreign_key 'group_expenses', 'groups'
+  add_foreign_key 'groups', 'users', column: 'author_id'
 end
