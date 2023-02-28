d967aab98e9a01057093317826f3df0118801fdb
masouma <masouma2020@gmail.com>
2023-02-28 08:49:22 +0430

    Modigy rubocop.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def after_sign_in_path_for(resource)
-      groups_path
-    end
-  
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password])
-    end
+  def after_sign_in_path_for(_resource)
+    groups_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
+  end
 end
