48a2fd430b6bd462bddf91b4062f243542ff48f5
masouma <masouma2020@gmail.com>
2023-02-28 14:51:53 +0430

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'splash_screen#splash'
+  get '/splash', to: 'splash_screen#splash'
+
+  resources :users
+  resources :groups, :path => "categories" do 
+    resources :expenses, :path => "transactions"
+  end
   devise_for :users
 
   devise_scope :user do
